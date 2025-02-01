e9f8f62b67a46fe29acf4491870da75944c6d32d
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-02-01 21:03:01 +0100

    retour en arrière pour le footer. il semble que la class pose problème

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,21 +1,22 @@
 "use client";
 
-import {Component} from "react";
+import { useState, useEffect } from "react";
 
-class Footer extends Component {
-    state = {
-        year: new Date().getFullYear(),
-    };
+const Footer = () => {
+    const [year, setYear] = useState(new Date().getFullYear());
 
-    render() {
-        return (
-            <div className="py-6">
-                <div className="mt-6 w-4/5 mx-auto text-center text-white opacity-70">
-                    &#169; Copyright Mathieu Stamm {this.state.year}
-                </div>
+    useEffect(() => {
+        const currentYear = new Date().getFullYear();
+        setYear(currentYear);
+    }, []);
+
+    return (
+        <div className="py-6">
+            <div className="mt-6 w-4/5 mx-auto text-center text-white opacity-70">
+                &#169; Copyright Mathieu Stamm {year}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Footer;
