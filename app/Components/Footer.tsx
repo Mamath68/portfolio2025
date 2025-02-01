ea649a1536f515a832320cb067aa81124b287fab
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-02-01 21:13:19 +0100

    correction, normalement, des erreurs

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import {Component} from "react";
 
-const Footer = () => {
-    const [year, setYear] = useState(new Date().getFullYear());
+class Footer extends Component {
+    state = {
+        year: new Date().getFullYear(),
+    };
 
-    useEffect(() => {
-        const currentYear = new Date().getFullYear();
-        setYear(currentYear);
-    }, []);
-
-    return (
-        <div className="py-6">
-            <div className="mt-6 w-4/5 mx-auto text-center text-white opacity-70">
-                &#169; Copyright Mathieu Stamm {year}
+    render() {
+        return (
+            <div className="py-6">
+                <div className="mt-6 w-4/5 mx-auto text-center text-white opacity-70">
+                    &#169; Copyright Mathieu Stamm {this.state.year}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Footer;
